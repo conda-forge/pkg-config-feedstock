99e2230527e6a444b1502d56b3a842c107ae9659
Isuru Fernando <isuruf@gmail.com>
2024-07-10 11:06:40 -0500

    set PKG_CONFIG_PATH on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,3 +8,12 @@ if errorlevel 1 exit 1
 
 copy release\x64\pkg-config.exe %LIBRARY_PREFIX%\bin\pkg-config.exe
 if errorlevel 1 exit 1
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+FOR %%F IN (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
+    if errorlevel 1 exit 1
+    copy %RECIPE_DIR%\scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if errorlevel 1 exit 1
+)
