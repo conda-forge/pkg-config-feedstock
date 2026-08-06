0ebc6ee22be904d9c4b55b5931d790de0014701f
H. Vetinari <h.vetinari@gmx.com>
2026-08-06 21:05:28 +1100

    preseed the cross-compilation cache values for riscv
    Co-Authored-By: bitsk <bigbits@hackx.cc>

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,18 @@ if [[ "$target_platform" == osx-* ]]; then
   export CFLAGS="${CFLAGS} -Wno-int-conversion"
 fi
 
+if [[ "$target_platform" == "linux-riscv64" ]]; then
+  # while bootstrapping riscv64 (with no `glib` available yet), rely on vendored copy,
+  # but preseed the cross-compilation cache values to avoid `./configure failed for glib`
+  export glib_cv_stack_grows=no
+  export glib_cv_working_bcopy=yes
+  export glib_cv_uscore=no
+  export ac_cv_func_posix_getpwuid_r=yes
+  export ac_cv_func_nonposix_getpwuid_r=no
+  export ac_cv_func_posix_getgrgid_r=yes
+  export ac_cv_func_nonposix_getgrgid_r=no
+fi
+
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "$target_platform" != "linux-riscv64" ]]; then
   export GLIB_CFLAGS="-I${PREFIX}/include/glib-2.0 -I${PREFIX}/lib/glib-2.0/include"
   export GLIB_LIBS="-L${PREFIX}/lib -lglib-2.0"
